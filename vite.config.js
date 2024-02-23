88af5852374ada350a5b7fb96b7817bd4d97888b
Macy Huang <mjmj3794@gmail.com>
2024-02-23 22:05:07 +0800

    新增scss

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,67 @@
-import 'dotenv/config';
+// import 'dotenv/config';
+// import { fileURLToPath, URL } from 'node:url';
+
+// import { defineConfig } from 'vite';
+// import vue from '@vitejs/plugin-vue';
+
+// // eslint-disable-next-line import/no-extraneous-dependencies
+// import eslintPlugin from 'vite-plugin-eslint';
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   base: '/vite-work6/',
+//   plugins: [
+//     vue(),
+//     eslintPlugin({
+//       include: ['src/**/*.js', 'src/**/*.vue', 'src/*.js', 'src/*.vue'],
+//     }),
+//   ],
+//   resolve: {
+//     alias: {
+//       '@': fileURLToPath(new URL('./src', import.meta.url)),
+//     },
+//   },
+// });
+
 import { fileURLToPath, URL } from 'node:url';
 
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
-// eslint-disable-next-line import/no-extraneous-dependencies
-import eslintPlugin from 'vite-plugin-eslint';
-
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? `/${process.env.REPOSITORY_NAME}/` : '/',
   plugins: [
     vue(),
-    eslintPlugin({
-      include: ['src/**/*.js', 'src/**/*.vue', 'src/*.js', 'src/*.vue'],
-    }),
   ],
+  base: '/vite-work6/dist',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 });
+
+// import 'dotenv/config'
+// import { fileURLToPath, URL } from 'node:url'
+
+// import { defineConfig } from 'vite'
+// import vue from '@vitejs/plugin-vue'
+
+// // eslint-disable-next-line import/no-extraneous-dependencies
+// import eslintPlugin from 'vite-plugin-eslint'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   base: process.env.NODE_ENV === 'production' ? `/${process.env.REPOSITORY_NAME}/` : '/',
+//   plugins: [
+//     vue(),
+//     eslintPlugin({
+//       include: ['src/**/*.js', 'src/**/*.vue', 'src/*.js', 'src/*.vue']
+//     })
+//   ],
+//   resolve: {
+//     alias: {
+//       '@': fileURLToPath(new URL('./src', import.meta.url))
+//     }
+//   }
+// })
